e8c46f95f4118c1417483cccb603da032aaadea5
cyberpig <1badcyberpig22@gmail.com>
2017-11-07 06:14:17 -0600

    Update ord_chr_proto.py

--- ord_chr_proto.py
+++ ord_chr_proto.py
@@ -1,7 +1,19 @@
-# prototype program under development
+# With this little code you type in a letter and number in you get the ord and chr output back. 
+fgh = True
+while fgh:
+    
+    print("1. enter letter and number")
+    print("2. Kill Program")
 
-a = input("enter a letter: ")
-b = int(input("enter a number: ")
+    chs =("+> ")
 
-print(ord(a))
-print(chr(b))
+    if chs == "1":
+        q = input("enter a letter: ")
+        w = int(input("enter a number: "))
+
+        print(ord(q))
+        print(chr(w))
+
+    if chs == "2":
+        print("later homes smell you later")
+        break
